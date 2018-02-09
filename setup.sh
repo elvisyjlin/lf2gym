5211f3c4f0e3f8e9098d05fd983cec54d7905c02
Elvis Yu-Jing Lin <linx1045@umn.edu>
2018-02-09 17:50:06 +0800

    modify setup (check before copy)

--- setup.sh
+++ setup.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/bash
 
+function copy {
+    md5_1=($(md5sum $1))
+    md5_2=($(md5sum $2))
+    if [ $a != $b ]; then
+        cp -v $1 $2
+    else
+        echo "'$2' is up-to-date."
+    fi
+}
+
 echo "Checking LF2 files..."
 if [ ! -d F.LF ]; then
     echo "F.LF/ does not exist, cloning one from Github..."
     git clone https://github.com/Project-F/F.LF.git
 fi
-
 pushd F.LF
-
 if [ ! -d LF2_19 ]; then
     echo "F.LF/LF2_19 does not exist, cloning one from Github..."
     git clone https://github.com/Project-F/LF2_19.git
 fi
-
 popd
 
 echo "Replacing modified files..."
 FROM="modify"
-cp -v   $FROM/controller.js       F.LF/core/controller.js
-cp -v   $FROM/game.html           F.LF/game/game.html
-cp -v   $FROM/game.js             F.LF/game/game.js
-cp -v   $FROM/global.js           F.LF/LF/global.js
-cp -v   $FROM/character.js        F.LF/LF/character.js
-cp -v   $FROM/match.js            F.LF/LF/match.js
-cp -v   $FROM/weapon.js           F.LF/LF/weapon.js
-cp -v   $FROM/background.js       F.LF/LF/background.js
-cp -v   $FROM/back3.png           F.LF/LF2_19/bg/hkc/back3.png
-cp -v   $FROM/message_overlay.png F.LF/LF2_19/UI/message_overlay.png
-cp -v   $FROM/bg.js               F.LF/LF2_19/bg/hkc/bg.js
+copy    $FROM/controller.js       F.LF/core/controller.js
+copy    $FROM/game.html           F.LF/game/game.html
+copy    $FROM/game.js             F.LF/game/game.js
+copy    $FROM/global.js           F.LF/LF/global.js
+copy    $FROM/character.js        F.LF/LF/character.js
+copy    $FROM/match.js            F.LF/LF/match.js
+copy    $FROM/weapon.js           F.LF/LF/weapon.js
+copy    $FROM/background.js       F.LF/LF/background.js
+copy    $FROM/back3.png           F.LF/LF2_19/bg/hkc/back3.png
+copy    $FROM/message_overlay.png F.LF/LF2_19/UI/message_overlay.png
+copy    $FROM/bg.js               F.LF/LF2_19/bg/hkc/bg.js
